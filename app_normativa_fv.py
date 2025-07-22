80d77af6f75f71f90c56a2418529dd19f619b946
Esteban Javier Navarro Rojas <esteban.navarro@cecltda.local>
2025-07-22 18:16:24 -0400

    Aplica parche para forzar uso de pysqlite3

--- app_normativa_fv.py
+++ app_normativa_fv.py
@@ -1,3 +1,9 @@
+# --- PARCHE PARA SQLITE ---
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# --------------------------
+
 import os
 import streamlit as st
 from langchain_community.vectorstores import Chroma
