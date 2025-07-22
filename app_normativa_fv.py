f482838cdf3e6e6457124aa5d08f000ecddb93dd
Esteban Javier Navarro Rojas <esteban.navarro@cecltda.local>
2025-07-22 18:12:24 -0400

    Actualiza importaciones de LangChain

--- app_normativa_fv.py
+++ app_normativa_fv.py
@@ -1,10 +1,10 @@
 import os
 import streamlit as st
-from langchain.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 # --- CONFIGURACIÓN DE LA PÁGINA ---
